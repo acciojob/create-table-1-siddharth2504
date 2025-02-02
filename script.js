9c3afaa6c3b39fe29ffae738685a3ece4bca6049
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-02-03 02:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function insert_Row() {
     //Write your code here
 	let insert = document.querySelector("#sampleTable");
 
-	insert.innerHTML = "<tr><td>New Cell1 </td> <td>New Cell2</td></tr>" + innerHTML;
+	let newRow = "<tr><td>New Cell1 </td> <td>New Cell2</td></tr>";
+	insert.innerHTML = newRow + innerHTML;
   
   
 }
