7b31a9d76af718c27747dba8fd951719eaa8912d
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-02-03 01:58:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert_Row() {
     //Write your code here
-	let insert = document.querySelector("#sampleTable")
+	let insert = document.querySelector("#sampleTable");
 
-	insert.innerHTML = "<tr><td>New Cell1 </td> <td>New Cell2</td></tr>"
+	insert.innerHTML = "<tr><td>New Cell1 </td> <td>New Cell2</td></tr>" + innerHTML;
   
   
 }
