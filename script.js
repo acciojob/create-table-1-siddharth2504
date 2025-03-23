05d6bd2d870ba1cf2fc0c4db1b8a9ef0a68ec644
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-03-23 18:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function insert_Row() {
-    //Write your code here
-	let insert = document.querySelector("#sampleTable");
+let btn = document.querySelector("button")
+let table = document.getElementById("sampleTable")
 
-	let newRow = "<tr><td>New Cell1 </td> <td>New Cell2</td></tr>";
-	insert.innerHTML = newRow + insert.innerHTML;
-  
-  
+btn.addEventListener("click", insertRow)
+
+function insertRow(){
+	let tr = document.createElement("tr")
+	let td1 = document.createElement("td")
+	td1.innerText = "New Cell1"
+	let td2 = document.createElement("td")
+	td2.innerText = "New Cell2"
+
+	tr.append(td1, td2)
+	table.prepend(tr)
 }
